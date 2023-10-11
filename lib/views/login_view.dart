8d4fc394c4fb6386dd58edc869fda63fffd32a64
censeapp <censeapp@gmail.com>
2023-10-11 11:16:24 +0300

    step-8

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -65,10 +65,19 @@ class _LoginViewState extends State<LoginView> {
                   await FirebaseAuth.instance.signInWithEmailAndPassword(
                       email: email, password: password);
 
-                  Navigator.of(context).pushNamedAndRemoveUntil(
-                    notesRoute,
-                    (route) => false,
-                  );
+                  final user = FirebaseAuth.instance.currentUser;
+                  if (user?.emailVerified ?? false) {
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                      notesRoute,
+                          (route) => false,
+                    );
+                  } else {
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                      verifyEmailRoute,
+                          (route) => false,
+                    );
+                  }
+
                 } on FirebaseAuthException catch (e) {
                   if (e.code == 'INVALID_LOGIN_CREDENTIALS') {
                     await showErrorDialog(
