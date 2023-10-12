3c0edf0b99beb6714c149e7c7fa430979510c042
censeapp <censeapp@gmail.com>
2023-10-12 18:13:25 +0300

    step-10

--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -1,14 +1,14 @@
 import "package:mynotes/services/auth/auth_provider.dart";
 import "package:mynotes/services/auth/auth_user.dart";
 import "package:mynotes/services/auth/firebase_auth_provider.dart";
-import 'package:firebase_core/firebase_core.dart';
 
 class AuthService implements AuthProvider {
   final AuthProvider provider;
   const AuthService(this.provider);
 
-
   factory AuthService.firebase() => AuthService(FirebaseAuthProvider());
+
+ 
 
   @override
   Future<AuthUser> createUser({
